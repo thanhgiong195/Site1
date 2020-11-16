802bc5bc9f7923e1132c37f770d7efe0d8baac5e
Thanh Giong <giong.bethanh@vietis.com.vn>
2020-11-16 19:38:19 +0700

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fb271f6b83a2f001f2df73f",
+				"5fb271f6b83a2f001f2df741"
+			]
+		},
+		"5fb271f6b83a2f001f2df73f": {
+			"id": "5fb271f6b83a2f001f2df73f",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fb271f6b83a2f001f2df741": {
+			"id": "5fb271f6b83a2f001f2df741",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
